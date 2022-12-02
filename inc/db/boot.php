0814d90aa1a7f858acb6d7b2844095309f54848e
sunkang <13127955213@qq.com>
2022-12-02 11:39:01 +0800

    v1.0.2
    优化DB

--- inc/db/boot.php
+++ inc/db/boot.php
@@ -1,66 +1,12 @@
 <?php 
-/*
-    Copyright (c) 2021-2031, All rights reserved.
-    This is  a library, use is under MIT license.
-*/
-/**
-* 以 CodeIgniter 为例
-*/
-include APPPATH.'config/database.php';
-$dbconfig = $db['default'];
-$medoo_db_config['db_dsn'] = $dbconfig['dsn'];
-$medoo_db_config['db_user'] = $dbconfig['username'];
-$medoo_db_config['db_pwd'] = $dbconfig['password'];
-
-/*
-include APPPATH.'db_connect.php';  
-$res = db_pager("products",'*',['LIMIT'=>2]);
-echo db_pager_html();
-print_r($res);
-*/
- 
-/**
-//saas系统管理平台数据库
-$medoo_db_config['main_db_host'] = '127.0.0.1';
-$medoo_db_config['main_db_name'] = 'main';
-$medoo_db_config['main_db_user'] = 'root';
-$medoo_db_config['main_db_pwd'] = '111111';
-$medoo_db_config['main_db_port'] = 3306; 
-
-// 从库读库 
-$medoo_db_config['read_db_host'] = '127.0.0.1';
-$medoo_db_config['read_db_name'] = ['read2','read1'];
-$medoo_db_config['read_db_user'] = 'root';
-$medoo_db_config['read_db_pwd'] = '111111';
-$medoo_db_config['read_db_port'] = 3306; 
-
-db_active_main();
-可以调用 db_get等方法
-
-db_active_default();
-
-db_active_read();
-可以调用 db_get等方法
-
-db_active_default();
-
-测试
-db_active_main(); 
-$r = db_get('a','*',[]);
-pr($r);
-db_active_default();
-
-db_active_read(); 
-//db_insert('a',['title'=>'read'.time()]);
-$r = db_get('a','*',[]);
-pr($r);
-db_active_default();
-
-
- */
+include __DIR__.'/Medoo.php';
+include __DIR__.'/function.php';
 global $_db,$_db_active,$_db_connects; 
 //连接默认数据库  
-if($medoo_db_config['db_dsn'] && $medoo_db_config['db_user'] && $medoo_db_config['db_pwd']){ 
+if($medoo_db_config['db_user'] && $medoo_db_config['db_pwd']){ 
+    if(!isset($medoo_db_config['db_dsn'])){
+        $medoo_db_config['db_dsn'] = "mysql:dbname={$medoo_db_config['db_name']};host={$medoo_db_config['db_host']};port={$medoo_db_config['db_port']}";
+    }
     try {
         $pdo = new PDO($medoo_db_config['db_dsn'], $medoo_db_config['db_user'], $medoo_db_config['db_pwd']);
         $_db = new Medoo\Medoo([
